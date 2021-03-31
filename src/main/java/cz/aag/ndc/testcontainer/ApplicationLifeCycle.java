a1959be2559547708f9dc124edc7ac63eee9c809
barenode <frantisek.hylmar@gmail.com>
2021-03-31 15:24:24 +0200

    release

--- src/main/java/cz/aag/ndc/testcontainer/ApplicationLifeCycle.java
+++ src/main/java/cz/aag/ndc/testcontainer/ApplicationLifeCycle.java
@@ -21,8 +21,6 @@ public class ApplicationLifeCycle {
         System.out.println(" |_|   |_|  \\___/ \\_/ |_|\\__,_|\\___|_|             ");
         System.out.println("                                                   ");                                                
         System.out.println("Provider is starting with profile " + ProfileManager.getActiveProfile());   
-        System.out.println("Provider is starting with profile " + ProfileManager.getActiveProfile());   
-        System.out.println("Provider is starting with profile " + ProfileManager.getActiveProfile());   
         System.out.println("                                                   ");                
     }
     
