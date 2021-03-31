e89da73d14e4dc91b6ae668f74a020f316f81821
barenode <frantisek.hylmar@gmail.com>
2021-03-31 17:04:22 +0200

    Update ApplicationLifeCycle.java

--- src/main/java/cz/aag/ndc/testcontainer/ApplicationLifeCycle.java
+++ src/main/java/cz/aag/ndc/testcontainer/ApplicationLifeCycle.java
@@ -20,9 +20,7 @@ public class ApplicationLifeCycle {
         System.out.println(" |  __/| | | (_) \\ V /| | (_| |  __/ |             ");
         System.out.println(" |_|   |_|  \\___/ \\_/ |_|\\__,_|\\___|_|             ");
         System.out.println("                                                   ");                                                
-        System.out.println("Provider is starting with profile " + ProfileManager.getActiveProfile());   
-        System.out.println("Provider is starting with profile " + ProfileManager.getActiveProfile());   
-        System.out.println("Provider is starting with profile " + ProfileManager.getActiveProfile());   
+        System.out.println("Provider is starting with profile " + ProfileManager.getActiveProfile());    
         System.out.println("                                                   ");                
     }
     
